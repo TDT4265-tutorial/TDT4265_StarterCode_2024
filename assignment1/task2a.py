53d88de30eff5d6949158d39d5df8dc30b01859c
eirivarn <eirik.varnes@gmail.com>
2024-02-08 13:01:43 +0100

    Task 2a sorted

--- assignment1/task2a.py
+++ assignment1/task2a.py
@@ -10,10 +10,12 @@ def pre_process_images(X: np.ndarray):
     Returns:
         X: images of shape [batch size, 785] in the range (-1, 1)
     """
-    assert X.shape[1] == 784,\
+    assert X.shape[1] == 784, \
         f"X.shape[1]: {X.shape[1]}, should be 784"
     # TODO implement this function (Task 2a)
-    return X
+    X_normalized = (X/255)*2 - 1
+    X_processed = np.c_[X_normalized, np.ones(X_normalized.shape[0])]
+    return X_processed
 
 
 def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray) -> float:
@@ -25,18 +27,22 @@ def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray) -> float:
         Cross entropy error (float)
     """
     # TODO implement this function (Task 2a)
-    assert targets.shape == outputs.shape,\
+    assert targets.shape == outputs.shape, \
         f"Targets shape: {targets.shape}, outputs: {outputs.shape}"
-    return 0
+
+    cross_entropy_loss = -np.mean(targets * np.log(outputs) +
+                                  (1 - targets) * np.log(1 - outputs))
+
+    return cross_entropy_loss
 
 
 class BinaryModel:
 
     def __init__(self):
         # Define number of input nodes
-        self.I = None
+        self.I = 785
         self.w = np.zeros((self.I, 1))
-        self.grad = None
+        self.grad = np.zeros_like(self.w)
 
     def forward(self, X: np.ndarray) -> np.ndarray:
         """
@@ -46,7 +52,9 @@ class BinaryModel:
             y: output of model with shape [batch size, 1]
         """
         # TODO implement this function (Task 2a)
-        return None
+        z = np.dot(X, self.w)
+        sigmoid = 1/(1+np.exp(-z))
+        return sigmoid
 
     def backward(self, X: np.ndarray, outputs: np.ndarray, targets: np.ndarray) -> None:
         """
@@ -57,14 +65,16 @@ class BinaryModel:
             targets: labels/targets of each image of shape: [batch size, 1]
         """
         # TODO implement this function (Task 2a)
-        assert targets.shape == outputs.shape,\
+        assert targets.shape == outputs.shape, \
             f"Output shape: {outputs.shape}, targets: {targets.shape}"
         self.grad = np.zeros_like(self.w)
-        assert self.grad.shape == self.w.shape,\
+        assert self.grad.shape == self.w.shape, \
             f"Grad shape: {self.grad.shape}, w: {self.w.shape}"
+        gradient = np.dot(X.T, outputs - targets)
+        self.grad = gradient / len(X)
 
     def zero_grad(self) -> None:
-        self.grad = None
+        self.grad = np.zeros_like(self.w)
 
 
 def gradient_approximation_test(model: BinaryModel, X: np.ndarray, Y: np.ndarray):
@@ -90,7 +100,7 @@ def gradient_approximation_test(model: BinaryModel, X: np.ndarray, Y: np.ndarray
         logits = model.forward(X)
         model.backward(X, logits, Y)
         difference = gradient_approximation - model.grad[i, 0]
-        assert abs(difference) <= epsilon**2,\
+        assert abs(difference) <= epsilon**2, \
             f"Calculated gradient is incorrect. " \
             f"Approximation: {gradient_approximation}, actual gradient: {model.grad[i,0]}\n" \
             f"If this test fails there could be errors in your cross entropy loss function, " \
@@ -105,7 +115,7 @@ def main():
     ) <= 1.0, f"The images (X_train) should be normalized to the range [-1, 1]"
     assert X_train.min() < 0 and X_train.min() >= - \
         1, f"The images (X_train) should be normalized to the range [-1, 1]"
-    assert X_train.shape[1] == 785,\
+    assert X_train.shape[1] == 785, \
         f"Expected X_train to have 785 elements per image. Shape was: {X_train.shape}"
 
     # Simple test for forward pass. Note that this does not cover all errors!
