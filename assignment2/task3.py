d593481cd52cc6069fe7920ab4040db095d07d8d
Ian Aksum <ian.aksum@gmail.com>
2024-02-23 21:07:36 +0100

    Changed neuroun count back to 64

--- assignment2/task3.py
+++ assignment2/task3.py
@@ -10,7 +10,7 @@ def main():
     num_epochs = 50
     learning_rate = .1
     batch_size = 32
-    neurons_per_layer = [128 , 10]
+    neurons_per_layer = [64 , 10]
     momentum_gamma = .9  # Task 3 hyperparameter
     shuffle_data = True
 
